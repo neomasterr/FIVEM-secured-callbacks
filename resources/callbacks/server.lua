3733c28c89f16c1bf4c2d46620336d52539a293b
Igor Alexandrovich <NEoMASTERR@mail.ru>
2018-07-27 13:10:39 +0400

    Init

---
+++ resources/callbacks/server.lua
@@ -0,0 +1,35 @@
+local callbacks = {}
+
+AddEventHandler('callback:register', function(name, callback, data)
+	if callback ~= nil and callback:sub(1, 9) ~= 'callback:' then
+		local key = randomString(8)
+		callbacks[key] = callback
+		TriggerEvent('callback:Registered:'..name, name, key, data)
+	else
+		print('bad callback:', callback)
+	end
+end)
+
+AddEventHandler('callback:secured', function(key, ...)
+	if callbacks[key] ~= nil then
+		TriggerEvent(callbacks[key], ...)
+	end
+end)
+
+function callbacks_encrypt(data)
+	local response = {}
+	for _, name in ipairs(data) do
+		response[name] = randomString(8)
+	end
+	return response
+end
+
+local charset = {} do
+    for c = 48, 57  do table.insert(charset, string.char(c)) end
+    for c = 97, 122 do table.insert(charset, string.char(c)) end
+end
+
+function randomString(length)
+    if not length or length <= 0 then return '' end
+    return randomString(length - 1) .. charset[math.random(1, #charset)]
+end
